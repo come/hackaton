4d16b9cea059560471cc934cdf8387a90ddff285
Benjamin <bguignabert@wanadev.fr>
2014-06-19 13:11:15 +0200

    computeanimation

--- OculusComponent3D.js
+++ OculusComponent3D.js
@@ -55,7 +55,7 @@ var OculusComponent3D = (function () {
     oculusComponent.prototype.moveTo = function(params) {
         var begin = params.begin;
         var end = params.end;
-        API.computeAnimation(this.camera, begin, end, {smooth: "linear"});
+        this.computeAnimation(this.camera, begin, end, {smooth: "linear"});
     }
 
     oculusComponent.prototype.moveBaby = function(positions) {
@@ -65,6 +65,95 @@ var OculusComponent3D = (function () {
                 end: positions[i+1]
             });
         }
+    };
+
+    oculusComponent.prototype.computeAnimation = function( target , src , dst , options ) {
+        options = options || {};
+
+        var   duration = Math.max( 0.01 , options.duration || 0 )
+            , callback = options.callback
+            , cleanAfterAnimation = typeof options.cleanAfterAnimation == 'undefined' ? true : options.cleanAfterAnimation
+            , name = options.name || "animation"
+            , isACamera = options.isACamera
+            , smoothFn = typeof options.smooth == 'function' ? options.smooth : smoothFns[ options.smooth || 'linear' ]
+
+        var anims = [];
+        for( var property in src ){
+
+            var animation = new BABYLON.Animation(
+                name+"_"+property,
+                property,
+                60,
+                src[ property ] instanceof BABYLON.Vector3 ? BABYLON.Animation.ANIMATIONTYPE_VECTOR3 : BABYLON.Animation.ANIMATIONTYPE_FLOAT,
+                BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT
+            );
+
+            var keys = [];
+
+            // compute key frames
+            var  km= (smoothFn==smoothFns.linear ? 2 : Math.max(2,duration/40))>>0 
+                ,r
+                ,v
+            for(var k=km;k--;){
+
+                r = k/(km-1);
+
+                var t = r===0?0:r===1?1 : smoothFn( r );
+
+                switch( animation.dataType ){
+                    case BABYLON.Animation.ANIMATIONTYPE_VECTOR3 :
+                        v = animation.vector3InterpolateFunction( src[ property ] , dst[ property ] , t );
+                    break;
+                    case BABYLON.Animation.ANIMATIONTYPE_FLOAT :
+                        v = animation.floatInterpolateFunction( src[ property ] , dst[ property ] , t );
+                    break;
+                }
+
+                keys.unshift({
+                    frame: r*100,
+                    value: v
+                })
+
+            }
+
+            animation.setKeys( keys );
+
+            anims.push( animation );
+        }
+
+        if( duration < 0.1 )
+            return new AnimationCancelor( anims , target );
+
+        // attach animations to the target
+        target.animations = target.animations.concat( anims )
+
+
+        var cancelor = new AnimationCancelor( anims , target );
+        cancelor._callBack = callback;
+
+        // ( ( 100 frame for the total animation ) / ( 60 frame per second ) ) * animationSpeed = duration
+        var animationSpeed = ( 100 / 60 ) / ( duration / 1000 )
+
+        // start the animation
+        target.getScene().beginAnimation( target , 0, 100 , false , animationSpeed , cancelor._onAnimationEnd.bind(cancelor) ); 
+
+
+        if( isACamera ){
+            //// as the object animated is a camera, camera.move event should be triggered
+            // let's register a function that will throw it every frame, and be killed with the cancelor
+            var eventThrower = function eventThrower(){
+                ujs.notify("wnp.engine3D.camera.move" );
+            }
+            var scene = target.getScene();
+
+            scene.registerBeforeRender( eventThrower )
+            cancelor._moreCleanUp = function(){
+                scene.unregisterBeforeRender( eventThrower );
+                eventThrower = null;
+            }
+        }
+
+        return cancelor;
     }
 
     return oculusComponent;
